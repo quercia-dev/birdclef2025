b5e4dc74eb1563749ddb3851ff9de494c4cf85a0
tebe-nigrelli <102675740+tebe-nigrelli@users.noreply.github.com>
2025-05-06 17:12:04 +0200

    add: cpu fix

---
+++ hpc_yamnet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name="train"
+#SBATCH --account=3189081
+#SBATCH --partition=ai
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=0
+#SBATCH --output=output/%x_%j.out # %x gives job name and %j gives job id
+#SBATCH --error=output/%x_%j.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=3189081@studbocconi.it
+
+cd ~/PRJ/birdclef2025/
+
+module load modules/miniconda3
+
+eval "$(conda shell.bash hook)"
+
+conda activate yamnet
+
+python yamnet.py
