8b600946aad949ac2db8c42282c5951a82674e6a
tebe-nigrelli <102675740+tebe-nigrelli@users.noreply.github.com>
2025-05-11 12:51:18 +0200

    exp: mass variation in efficient

--- hpc_train.sh
+++ hpc_train.sh
@@ -18,5 +18,5 @@ eval "$(conda shell.bash hook)"
 
 conda activate birdclef
 
-python model.py --model melcnn --epochs 3 --dataset labelled
-python model.py --model melcnn --epochs 3 --dataset yamnet
+python model.py --model efficient --epochs 10 --dataset labelled --mass 1.0
+python model.py --model efficient --epochs 10 --dataset labelled --mass 0.8
